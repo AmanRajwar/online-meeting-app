26253fd8fc526f8cb20a6b1f7c7ac0d68f4deccc
AmanRajwar <amanrajwar79@gmail.com>
2024-06-08 12:59:33 +0530

    navbar is done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
     <section className="sticky left-09 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
-          const isActive = pathname === link.route || pathname.startsWith(link.route);
+          const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
           return (
             <Link
               href={link.route}
